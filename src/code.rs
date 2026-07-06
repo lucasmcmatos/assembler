8e664e82f6df96c8c8b58dbb3e8990f9858b1c8d
Lucas Matos <lucasmmatos05@gmail.com>
2026-07-06 17:03:09 -0300

    feat(code): implementa tabela comp completa (a=0 e a=1).

--- src/code.rs
+++ src/code.rs
@@ -12,6 +12,39 @@ pub fn dest(dest: Option<&str>) -> &'static str {
     }
 }
 
+pub fn comp(comp: &str) -> &'static str {
+    match comp {
+        "0" => "0101010",
+        "1" => "0111111",
+        "-1" => "0111010",
+        "D" => "0001100",
+        "A" => "0110000",
+        "M" => "1110000",
+        "!D" => "0001101",
+        "!A" => "0110001",
+        "!M" => "1110001",
+        "-A" => "0110011",
+        "-M" => "1110011",
+        "D+1" => "0011111",
+        "A+1" => "0110111",
+        "M+1" => "1110111",
+        "D-1" => "0001110",
+        "A-1" => "0110010",
+        "M-1" => "1110010",
+        "D+A" => "0000010",
+        "D+M" => "1000010",
+        "D-A" => "0010011",
+        "D-M" => "1010011",
+        "A-D" => "0000111",
+        "M-D" => "1000111",
+        "D&A" => "0000000",
+        "D&M" => "1000000",
+        "D|A" => "0010101",
+        "D|M" => "1010101",
+        other => panic!("comp inválido: {other}"),
+    }
+}
+
 pub fn jump(jump: Option<&str>) -> &'static str {
     match jump {
         None => "000",
@@ -64,5 +97,50 @@ mod tests {
     #[should_panic(expected = "jump inválido")]
     fn panics_on_invalid_jump() {
         jump(Some("XYZ"));
+    }
+
+    #[test]
+    fn encodes_constants_and_d_with_a_equal_zero() {
+        assert_eq!(comp("0"), "0101010");
+        assert_eq!(comp("1"), "0111111");
+        assert_eq!(comp("-1"), "0111010");
+        assert_eq!(comp("D"), "0001100");
+        assert_eq!(comp("D+1"), "0011111");
+        assert_eq!(comp("D-1"), "0001110");
+    }
+
+    #[test]
+    fn encodes_a_variants_with_a_bit_zero() {
+        assert_eq!(comp("A"), "0110000");
+        assert_eq!(comp("!A"), "0110001");
+        assert_eq!(comp("-A"), "0110011");
+        assert_eq!(comp("A+1"), "0110111");
+        assert_eq!(comp("A-1"), "0110010");
+        assert_eq!(comp("D+A"), "0000010");
+        assert_eq!(comp("D-A"), "0010011");
+        assert_eq!(comp("A-D"), "0000111");
+        assert_eq!(comp("D&A"), "0000000");
+        assert_eq!(comp("D|A"), "0010101");
+        assert_eq!(comp("!D"), "0001101");
+    }
+
+    #[test]
+    fn encodes_m_variants_with_a_bit_one() {
+        assert_eq!(comp("M"), "1110000");
+        assert_eq!(comp("!M"), "1110001");
+        assert_eq!(comp("-M"), "1110011");
+        assert_eq!(comp("M+1"), "1110111");
+        assert_eq!(comp("M-1"), "1110010");
+        assert_eq!(comp("D+M"), "1000010");
+        assert_eq!(comp("D-M"), "1010011");
+        assert_eq!(comp("M-D"), "1000111");
+        assert_eq!(comp("D&M"), "1000000");
+        assert_eq!(comp("D|M"), "1010101");
+    }
+
+    #[test]
+    #[should_panic(expected = "comp inválido")]
+    fn panics_on_invalid_comp() {
+        comp("XYZ");
     }
 }
